060ccce4fd0b929ce28cb9089f0d095eae8c35d2
Daniel <byoung.mokk@gmail.com>
2026-01-13 22:29:07 +0900

    🩹Fix: 제출 코드 클래스명 변경

--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class FloydWarshall {
+public class Main {
 
 	public static void main(String[] args) throws Exception {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
